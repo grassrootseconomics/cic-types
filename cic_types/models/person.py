97f16ac8b2e29b86055f054d4edabaf81afc7455
Philip Wafula <philipwafula2@gmail.com>
2021-02-05 13:04:38 +0000

    Philip/refactor person type

--- cic_types/models/person.py
+++ cic_types/models/person.py
@@ -8,8 +8,11 @@ import vobject
 
 # local imports
 from cic_types.processor import phone_number_to_e164
-from cic_types.schemas.person import person_json_schema, vcard_json_schema
+from cic_types.schemas.load import load_validation_schema
 from cic_types.validator import validate_data
+
+person_json_schema = load_validation_schema(file_name='person.json')
+vcard_json_schema = load_validation_schema(file_name='vcard.json')
 
 
 class Person:
@@ -26,14 +29,12 @@ class Person:
     :type gender: str
     :cvar given_name: A user's given name.
     :type given_name: str
-    :cvar key: A list object containing key value pairs of tokens and addresses for users on the system.
-    :type key: list
-    :cvar latitude: Co-ordinate representing user's residence in latitude.
-    :type latitude: str
-    :cvar longitude: Co-ordinate representing user's residence in longitude.
-    :type longitude: str
-    :cvar selling: A list object representing businesses or services a user may offer and trade in on the system.
-    :type selling: list
+    :cvar identities: An object containing identity data for accounts on the system.
+    :type identities: dict
+    :cvar location: A dictionary containing location data.
+    :type location: dict
+    :cvar products: A list object representing businesses or services a user may offer and trade in on the system.
+    :type products: list
     :cvar tel: An E164 formatted phone number associated with a user's account.
     :type tel: str
     :raises ValidationError: if any value contravenes set validation parameters in the described json schemas.
@@ -43,16 +44,15 @@ class Person:
     family_name: str = None
     gender: str = None
     given_name: str = None
-    key: dict = None
-    latitude: float = None
-    longitude: float = None
-    selling: list = None
+    identities: dict = None
+    location: dict = None
+    products: list = None
     tel: str = None
 
     def __init__(self, person_data: dict):
         """
-        :param person_data:
-        :type person_data:
+        :param person_data: A dictionary object containing all metadata for the person type.
+        :type person_data: dict
         """
         self.person_data = person_data
 
@@ -69,10 +69,12 @@ class Person:
         self.family_name = v_card_data.get("family")
         self.gender = self.person_data.get("gender")
         self.given_name = v_card_data.get('given')
-        self.key = self.person_data.get("key")
-        self.latitude = self.person_data.get("location").get("latitude")
-        self.longitude = self.person_data.get("location").get("longitude")
-        self.selling = self.person_data.get("selling")
+        self.identities = person_data.get("identities")
+        self.location = {
+            "latitude": self.person_data.get("location").get("latitude"),
+            "longitude": self.person_data.get("location").get("longitude")
+        }
+        self.products = self.person_data.get("products")
         self.tel = v_card_data.get("tel")
 
     def serialize(self):
@@ -83,13 +85,9 @@ class Person:
         person_data = {
             "date_registered": self.date_registered,
             "gender": self.gender,
-            "key": self.key,
-            "location": {
-                "latitude": self.latitude,
-                "longitude": self.longitude,
-                "external": {}
-            },
-            "selling": self.selling,
+            "identities": self.identities,
+            "location": self.location,
+            "products": self.products,
             "vcard": generate_vcard_from_contact_data(
                 email=self.email,
                 family_name=self.family_name,
@@ -109,7 +107,7 @@ def generate_metadata_pointer(identifier: bytes, cic_type: str):
     :type identifier: bytes
     :param cic_type: type descriptor for cic specific objects.
     :type cic_type: str
-    :return: A sha256 hash of a blockchain address and cic-type.
+    :return: A sha256 hash of an identifier and cic-type.
     :rtype: str
     """
     hash_object = hashlib.new("sha256")
@@ -176,3 +174,36 @@ def generate_vcard_from_contact_data(email: str, family_name: str, given_name: s
     v_card_serialized = v_card.serialize()
     v_card_base64 = base64.b64encode(v_card_serialized.encode(encoding="utf-8"))
     return v_card_base64.decode(encoding="utf-8")
+
+
+def manage_identity_data(blockchain_address: str, chain_spec: str, blockchain_type: str, identity_data: dict = None):
+    """This function implements a management strategy for the identities held in metadata for an account, it creates
+    a dictionary object that stores the network and child network id against a list of addresses associated with an
+    account.
+    :param blockchain_address: An address on a wallet.
+    :type blockchain_address: str
+    :param chain_spec: The chain name and network id.
+    :type chain_spec: str
+    :param blockchain_type: The blockchain network on which the account exists.
+    :type blockchain_type: str
+    :param identity_data: A dictionary object containing data on the identities.
+    :type identity_data: dict
+    :return: A new or edited version of the identity data dictionary object.
+    :rtype: dict
+    """
+    addresses = []
+    if not identity_data:
+        identity_data = {
+            blockchain_type: {
+                chain_spec: addresses + [blockchain_address]
+            }
+        }
+        return identity_data
+    else:
+        if identity_data.get(blockchain_type):
+            addresses = identity_data.get(blockchain_type).get(chain_spec) or []
+
+        identity_data[blockchain_type] = {
+            chain_spec: addresses + [blockchain_address]
+        }
+        return identity_data
--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -10,23 +10,21 @@ import pytest
 def sample_person_metadata():
     return {
         "date_registered": 1573505158,
-        "vcard": "QkVHSU46VkNBUkQNClZFUlNJT046My4wDQpFTUFJTDpqb25zbm93QHRoZW5vcnRoLmNvbQ0KRk46Sm9uXCwgU25vdw0KTjpTbm93O0pvbjs7Ow0KVEVMO1RZUD1DRUxMOisyNTQ3MDAwMDAwMDANCkVORDpWQ0FSRA0K",
         "gender": "male",
-        "key": {
-            "ethereum": [
-                "0x16ba1a17650c4001e2fa28c6f883aabe8f90d5b7",
-                "0xE978b4A817e95Ca7F04370B83d631b8fea8E40D5"
-            ]
+        "identities": {
+            "evm": {
+                "Bloxberg:8995": [
+                    "0x16ba1a17650c4001e2fa28c6f883aabe8f90d5b7",
+                    "0xE978b4A817e95Ca7F04370B83d631b8fea8E40D5"
+                ]
+            }
         },
         "location": {
             "latitude": "-13.7932245",
             "longitude": "159.183828",
-            "external": {}
         },
-        "selling": [
-            "education",
-            "transport"
-        ]
+        "products": ["education", "transport"],
+        "vcard": "QkVHSU46VkNBUkQNClZFUlNJT046My4wDQpFTUFJTDpqb25zbm93QHRoZW5vcnRoLmNvbQ0KRk46Sm9uXCwgU25vdw0KTjpTbm93O0pvbjs7Ow0KVEVMO1RZUD1DRUxMOisyNTQ3MDAwMDAwMDANCkVORDpWQ0FSRA0K"
     }
 
 
