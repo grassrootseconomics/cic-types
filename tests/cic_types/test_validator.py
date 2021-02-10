1c9afa6185f36a9a85454cbd61d0d89b7009713a
PhilipWafula <philipwafula2@gmail.com>
2021-02-10 18:29:18 +0300

    Refactor testing to get it working.

--- tests/cic_types/test_validator.py
+++ tests/cic_types/test_validator.py
@@ -13,4 +13,4 @@ def test_invalid_data(sample_vcard_data):
     with pytest.raises(ValidationError) as error:
         sample_vcard_data['tel'] = 123456789
         validate_data(instance=sample_vcard_data, schema=vcard_json_schema)
-    assert str(error.value) == "123456789 is not of type 'string'\n\nFailed validating 'type' in schema['properties']['tel']:\n    {'pattern': '^([+254]?[0-9]{12})$', 'type': 'string'}\n\nOn instance['tel']:\n    123456789"
+    assert str(error.value) == "123456789 is not of type 'string'\n\nFailed validating 'type' in schema['properties']['tel']:\n    {'pattern': '^(\\\\+?[0-9]+)$', 'type': 'string'}\n\nOn instance['tel']:\n    123456789"
